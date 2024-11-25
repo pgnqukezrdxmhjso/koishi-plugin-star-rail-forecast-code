135fe8f0c3432ea8c400bc82e16b1b128a3d1b17
173gvQY8MFaDk9Jt <g@v.q>
2024-11-26 05:18:02 +0800

    Add notification function

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,45 @@
-import { Context, Logger, Schema } from "koishi";
-import packageJson from "../package.json";
-
-export const logger = new Logger(packageJson.name);
-
+import { Context, Schema } from "koishi";
+// noinspection ES6UnusedImports
+import {} from "koishi-plugin-message-topic-service";
+// noinspection ES6UnusedImports
+import {} from "koishi-plugin-cron";
 import Code from "./Code";
 
 export const name = "star-rail-forecast-code";
+
+export const inject = ["messageTopicService", "cron"];
 
 export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
+  ctx.messageTopicService.registerTopic("星铁.前瞻.开播").then();
+  ctx.cron("0 19 * * *", async () => {
+    try {
+      let act = await Code.getActData({ http: ctx.http });
+      const createdAt = act.data.post.created_at;
+      if (createdAt) {
+        const date = new Date(
+          +(createdAt + ((createdAt + "").length < 13 ? "000" : "")),
+        );
+        date.setHours(23, 59, 59, 999);
+        if (date.getTime() < Date.now()) {
+          return;
+        }
+      }
+      ctx.messageTopicService
+        .sendMessageToTopic("星铁.前瞻.开播", "今天有星铁前瞻！")
+        .then();
+    } catch (e) {}
+  });
+
   ctx.command("星铁前瞻").action(async ({ session }) => {
     try {
       const msg = await Code.get({ http: ctx.http });
       await session.send(msg);
     } catch (e) {
-      logger.error(e);
+      ctx.logger.error(e);
       await session.send(e.eMsg ? e.eMsg : "获取失败");
     }
   });
