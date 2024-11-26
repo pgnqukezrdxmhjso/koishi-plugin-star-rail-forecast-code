c79460a31d63c47a0279e16d533c2ebbfddec6ca
173gvQY8MFaDk9Jt <g@v.q>
2024-11-26 18:40:23 +0800

    a

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,15 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  ctx.messageTopicService.registerTopic("星铁.前瞻.开播").then();
+  const topic = "星铁.前瞻.开播";
+  ctx.messageTopicService.registerTopic(topic).then();
   ctx.cron("0 19 * * *", async () => {
     try {
+      const rows =
+        await ctx.messageTopicService.getTopicSubscribeByTopic(topic);
+      if (rows.length < 1) {
+        return;
+      }
       let act = await Code.getActData({ http: ctx.http });
       const createdAt = act.data.post.created_at;
       if (createdAt) {
@@ -29,16 +35,18 @@ export function apply(ctx: Context) {
         }
       }
       ctx.messageTopicService
-        .sendMessageToTopic("星铁.前瞻.开播", "今天有星铁前瞻！")
+        .sendMessageToTopic(topic, "今天有星铁前瞻！")
         .then();
-    } catch (e) {}
+    } catch (e) {
+      ctx.logger.error(e);
+    }
   });
   ctx.command("星铁前瞻订阅").action(async ({ session }) => {
     await ctx.messageTopicService.topicSubscribe({
       platform: session.bot.platform,
       selfId: session.bot.selfId,
       channelId: session.channelId,
-      bindingKey: "星铁.前瞻.开播",
+      bindingKey: topic,
       enable: true,
     });
     return "订阅成功";
@@ -48,7 +56,7 @@ export function apply(ctx: Context) {
       platform: session.bot.platform,
       selfId: session.bot.selfId,
       channelId: session.channelId,
-      bindingKey: "星铁.前瞻.开播",
+      bindingKey: topic,
       enable: false,
     });
     return "不订阅成功";
